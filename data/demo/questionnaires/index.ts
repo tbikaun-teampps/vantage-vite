289a25d745666db8ff703ac41d3e77bd529c8c48
tbikaun-teampps <tbikaun@teampps.com.au>
2025-09-11 08:16:30 +0800

    feat: add Mining Operations Readiness Assessment questionnaire

--- data/demo/questionnaires/index.ts
+++ data/demo/questionnaires/index.ts
@@ -2,11 +2,13 @@
 import { questionnaire as digitalWorkMgmtReadinessAssessment } from "./digital_work_mgmt_readiness_assessment.ts";
 import { questionnaire as weeklyPlannerChecklist } from "./weekly_planner_checklist.ts";
 import { questionnaire as iso55000Journey } from "./iso55000_journey.ts";
+import { questionnaire as wmPreAssessmentQuestionnaire } from "./wm_pre_assessment_questionnaire.ts";
 
 export {
   digitalWorkMgmtReadinessAssessment,
   weeklyPlannerChecklist,
   iso55000Journey,
+  wmPreAssessmentQuestionnaire,
 };
 
 // Array of all questionnaires for easy iteration
@@ -14,6 +16,7 @@ export const allQuestionnaires = [
   digitalWorkMgmtReadinessAssessment,
   weeklyPlannerChecklist,
   iso55000Journey,
+  wmPreAssessmentQuestionnaire,
 ] as const;
 
 // Default export for backwards compatibility
