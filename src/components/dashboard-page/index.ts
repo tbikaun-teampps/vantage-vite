adccdcddc43a43d6718ebb2db459a1c51255ae79
tbikaun-teampps <tbikaun@teampps.com.au>
2025-10-06 15:12:00 +0800

    add knip. remove unused dependencies. remove unused exports.

--- src/components/dashboard-page/index.ts
+++ src/components/dashboard-page/index.ts
@@ -1,4 +1,2 @@
 export { DashboardPage } from './dashboard-page';
-export { BackButton } from './back-button';
 export type { DashboardPageProps } from './types';
-export { BackButtonDynamic } from './back-button-dynamic';
