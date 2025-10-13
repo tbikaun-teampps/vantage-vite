1cb4f1da20813e7c865f8afce68fc9e695c79224
tbikaun-teampps <tbikaun@teampps.com.au>
2025-10-13 15:56:22 +0800

    remove unused entry point discovery logic from build script

--- server/build.js
+++ server/build.js
@@ -1,25 +1,4 @@
 import esbuild from "esbuild";
-import { readdir } from "fs/promises";
-import { join } from "path";
-
-async function getEntryPoints(dir) {
-  const entries = [];
-  const files = await readdir(dir, { withFileTypes: true, recursive: true });
-
-  for (const file of files) {
-    if (
-      file.isFile() &&
-      file.name.endsWith(".ts") &&
-      !file.name.endsWith(".d.ts")
-    ) {
-      entries.push(join(file.path, file.name));
-    }
-  }
-
-  return entries;
-}
-
-const entryPoints = await getEntryPoints("./src");
 
 await esbuild.build({
   entryPoints: ["./src/index.ts"], // Single entry point
