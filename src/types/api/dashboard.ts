b14d838ba9770024bacd261045d5bd8b8a5b4abb
tbikaun-teampps <tbikaun@teampps.com.au>
2025-11-04 10:29:58 +0800

    add more details to item response

--- src/types/api/dashboard.ts
+++ src/types/api/dashboard.ts
@@ -1,4 +1,7 @@
 import type { paths } from ".";
 
 export type ActivityWidgetResponseData =
-  paths["/dashboards/widgets/{companyId}/activity"]["get"]["responses"]["200"]["content"]["application/json"]['data'];
+  paths["/dashboards/widgets/{companyId}/activity"]["get"]["responses"]["200"]["content"]["application/json"]["data"];
+
+export type ActivityWidgetListItem =
+  ActivityWidgetResponseData["items"][number];
